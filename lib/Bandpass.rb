1edee9f4a6a0db3f1335a004a1ab7139a4449836
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-11-18 18:04:17 +0000

    Added test to ouput frequencies as required.

--- lib/Bandpass.rb
+++ lib/Bandpass.rb
@@ -1,7 +1,19 @@
 class Bandpass
 
   def freq_analysis(min, max, frequencies)
-    return min
+    min = min
+    max = max
+    new_frequencies = []
+    frequencies.each do |freq|
+      if freq < min
+        new_frequencies << min
+      elsif freq > max
+        new_frequencies << max
+      else
+        new_frequencies << freq
+      end
+    end
+    return new_frequencies
   end
 
 end
--- spec/Bandpass_spec.rb
+++ spec/Bandpass_spec.rb
@@ -3,9 +3,16 @@ require 'Bandpass'
 describe Bandpass do
 
   describe 'testing input of min, max and array of frequencies' do
-    it 'takes in an integer for minimum and maximum and returns both' do
+    it 'responds to 3 arguments' do
     newfilter = Bandpass.new
     expect(newfilter).to respond_to(:freq_analysis).with(3).arguments
+  end
+end
+
+  describe '#freq_analysis' do
+    it 'takes 3 arguments and returns all' do
+    newfilter = Bandpass.new
+    expect(newfilter.freq_analysis(20, 1200, [10, 20, 1200, 1500])).to eq([20, 20, 1200, 1200])
   end
 end
 
